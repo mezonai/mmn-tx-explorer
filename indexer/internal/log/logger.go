0cb749b5eb22509d1bda7226e824042e2b7b3f92
Huỳnh Kim Huy <huy.huynhkim@ncc.asia>
2025-09-10 11:08:02 +0700

    add file log, protail push log to grafana

--- indexer/internal/log/logger.go
+++ indexer/internal/log/logger.go
@@ -1,7 +1,9 @@
 package log
 
 import (
+	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -23,10 +25,58 @@ func NewLogger(name string) zerolog.Logger {
 	}
 	zerolog.SetGlobalLevel(level)
 
-	logger := zerolog.New(os.Stderr).With().Timestamp().Str("component", name).Logger()
-	logger = logger.With().Caller().Logger()
+	// Create multi-writer for both console and file output
+	var writers []io.Writer
+	
+	// Console output (always enabled)
+	consoleWriter := os.Stderr
 	if config.Cfg.Log.Prettify {
-		logger = logger.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+		writers = append(writers, zerolog.ConsoleWriter{Out: consoleWriter})
+	} else {
+		writers = append(writers, consoleWriter)
 	}
+	
+	// File output (if enabled)
+	if config.Cfg.Log.FileEnabled && config.Cfg.Log.FilePath != "" {
+		// Ensure log directory exists
+		logDir := filepath.Dir(config.Cfg.Log.FilePath)
+		if err := os.MkdirAll(logDir, 0755); err != nil {
+			log.Warn().Err(err).Msg("Failed to create log directory, falling back to console only")
+		} else {
+			// Create log file with rotation support
+			fileWriter, err := createFileWriter(config.Cfg.Log)
+			if err != nil {
+				log.Warn().Err(err).Msg("Failed to create file writer, falling back to console only")
+			} else {
+				writers = append(writers, fileWriter)
+			}
+		}
+	}
+	
+	// Create multi-writer
+	var output io.Writer
+	if len(writers) == 1 {
+		output = writers[0]
+	} else {
+		output = zerolog.MultiLevelWriter(writers...)
+	}
+
+	logger := zerolog.New(output).With().Timestamp().Str("component", name).Logger()
+	logger = logger.With().Caller().Logger()
+	
 	return logger
+}
+
+// createFileWriter creates a file writer with rotation support
+func createFileWriter(logConfig config.LogConfig) (io.Writer, error) {
+	// For now, we'll use a simple file writer
+	// In production, you might want to use lumberjack for rotation
+	file, err := os.OpenFile(logConfig.FilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+	
+	// Note: For production use, consider implementing log rotation
+	// using a library like gopkg.in/natefinch/lumberjack.v2
+	return file, nil
 }
