bdf5747e6b76fe3d4ff6c21871820e4edf51da85
Nguyen Son Tung <tung.nguyen@ncc.asia>
2025-10-08 18:45:57 +0700

    update FE env

--- frontend/service/index.ts
+++ frontend/service/index.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const isServer = typeof window === 'undefined';
 const baseURL = isServer
-  ? process.env.APP_API_URL_INTERNAL || 'http://localhost:8080'
-  : process.env.NEXT_PUBLIC_APP_API_URL;
+  ? "https://dev-mmn.nccsoft.vn/indexer-api" || 'http://localhost:8080'
+  : "https://dev-mmn.nccsoft.vn/indexer-api";
 
 const apiClient = axios.create({
   baseURL,
