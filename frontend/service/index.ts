7f9b896318ffbd19c9004243dde7ea288133d047
tuananhncc <anh.nguyentuan2@ncc.asia>
2025-10-08 21:58:56 +0700

    80-ci-cd

--- frontend/service/index.ts
+++ frontend/service/index.ts
@@ -6,7 +6,7 @@ const baseURL = isServer
   : process.env.NEXT_PUBLIC_APP_API_URL;
 
 const apiClient = axios.create({
-  baseURL: "https://dev-mmn.nccsoft.vn/indexer-api",
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
