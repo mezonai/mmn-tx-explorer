d113b8b09f5d63f47ef9635266b934d2215c8751
phuongtrinhviet <phuong.trinhviet@ncc.asia>
2025-10-09 09:11:34 +0700

    update

--- indexer/internal/tools/postgres/0012_create_user_transaction_table.down.sql
+++ indexer/internal/tools/postgres/0012_create_user_transaction_table.down.sql
@@ -1,8 +1,6 @@
 -- Drop indexes first
-DROP INDEX IF EXISTS idx_user_transaction_address;
-DROP INDEX IF EXISTS idx_user_transaction_hash;
-DROP INDEX IF EXISTS idx_user_transaction_timestamp;
-DROP INDEX IF EXISTS idx_user_transaction_address_type;
+DROP INDEX IF EXISTS idx_user_transaction_address_timestamp;
+DROP INDEX IF EXISTS idx_user_transaction_addr_type_time;
 
 -- Drop the user_transaction table
 DROP TABLE IF EXISTS user_transaction;
