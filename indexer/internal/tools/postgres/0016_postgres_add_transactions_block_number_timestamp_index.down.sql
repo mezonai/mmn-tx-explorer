4411ae709fb7d46eef6ab36d6758412dd7a0b632
phuongtrinhviet <phuong.trinhviet@ncc.asia>
2025-10-15 17:15:50 +0700

    add index

--- indexer/internal/tools/postgres/0016_postgres_add_transactions_block_number_timestamp_index.down.sql
+++ indexer/internal/tools/postgres/0016_postgres_add_transactions_block_number_timestamp_index.down.sql
@@ -1,3 +1,7 @@
 -- Drop composite index for block_number + transaction_timestamp DESC
 
 DROP INDEX IF EXISTS idx_transactions_block_number_timestamp;
+
+-- Drop composite index for transaction_timestamp + hash DESC
+
+DROP INDEX IF EXISTS idx_transactions_timestamp_hash;
