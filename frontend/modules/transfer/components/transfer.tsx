bbe3469f178f266b7da4fe06cc3eaa9c47d0cbb6
Arthur Do <athur4c@gmail.com>
2025-10-23 15:49:27 +0700

    replace the initial mezon token name

--- frontend/modules/transfer/components/transfer.tsx
+++ frontend/modules/transfer/components/transfer.tsx
@@ -102,16 +102,16 @@ export const Transfer = () => {
     <div className="h-full w-full px-4 sm:px-6 lg:px-8">
       <PageHeader
         title="Give Coffee"
-        header="Send tokens to another address."
+        header="Send Mezon Đồng to another address."
         description="Show some love—send a coffee to support someone’s day."
       />
       <div className="border-primary/50 mx-auto mt-8 w-full max-w-3xl rounded-3xl border bg-white/5 p-6 text-left shadow-md dark:border-white/10">
         <div className="mx-auto grid max-w-3xl gap-3 text-sm sm:grid-cols-1">
           <div className="bg-primary/10 shadow-primary/20 flex flex-col rounded-2xl border-none px-2 py-2 text-left shadow-md">
             <div className="text-primary dark:text-primary-light shadow-primary/20 px-2 text-left text-lg text-sm font-semibold">
-              Send Mezon Token
+              Send Mezon Đồng
             </div>
-            <p className="text-primary-light mt-2 px-2 text-left text-sm text-xs">Transfer tokens wallet-to-wallet</p>
+            <p className="text-primary-light mt-2 px-2 text-left text-sm text-xs">Transfer funds wallet-to-wallet</p>
           </div>
           <div className="hidden rounded-2xl px-2 py-2 text-left">
             <div className="text-gray hover:border-primary/40 px-2 text-left text-lg text-sm font-semibold transition hover:text-white">
@@ -123,9 +123,9 @@ export const Transfer = () => {
 
         <div className="mx-auto mt-2 grid max-w-3xl gap-8 py-7">
           <div className="bg-primary/10 border-primary/40 shadow-primary/20 flex flex-col rounded-3xl border p-6 text-left shadow-lg lg:p-8">
-            <div className="text-primary dark:text-primary-light text-left text-lg font-semibold">Send Mezon Token</div>
+            <div className="text-primary dark:text-primary-light text-left text-lg font-semibold">Send Mezon Đồng</div>
             <p className="text-primary-light/80 mt-2 text-left text-sm">
-              Tokens are transferred instantly once the transaction is confirmed on-chain.
+              Funds are transferred instantly once the transaction is confirmed on-chain.
             </p>
             <div className="mt-6 flex-1 space-y-5 text-sm text-gray-200">
               <div>
