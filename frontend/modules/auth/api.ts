b76e139230f2209504a6e64f7b4c48e91a359de6
tuyen-nguyenngoc <tuyen.nguyenngoc@ncc.asia>
2025-10-27 18:04:09 +0700

    Add new ui component

--- frontend/modules/auth/api.ts
+++ frontend/modules/auth/api.ts
@@ -2,17 +2,24 @@ import axios, { AxiosResponse } from 'axios';
 import { AUTHENTICATION_ENDPOINT } from './constants';
 import { LoginResponse } from './type';
 import { apiDongClient } from '@/service';
+import { STORAGE_KEYS } from '@/constant';
 export class AuthenticationService {
   static async getUserInfo(code: string): Promise<LoginResponse> {
     const response: AxiosResponse<LoginResponse> = await axios.get(AUTHENTICATION_ENDPOINT.USER_INFO, {
       params: { code },
     });
+
+    apiDongClient.defaults.headers.common['Authorization'] = `Bearer ${response.data.auth_token}`;
+
     return response.data;
   }
   static async refreshLogin(refreshToken: string): Promise<LoginResponse> {
     const response: AxiosResponse<LoginResponse> = await apiDongClient.post(AUTHENTICATION_ENDPOINT.REFRESH, {
       refresh_token: refreshToken,
     });
+
+    apiDongClient.defaults.headers.common['Authorization'] = `Bearer ${response.data.access_token}`;
+
     return response.data;
   }
 }
