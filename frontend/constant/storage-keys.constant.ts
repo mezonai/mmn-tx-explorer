d9da1d27806654ea0c3f4a6569081c645a8f8815
tuyen-nguyenngoc <tuyen.nguyenngoc@ncc.asia>
2025-10-28 17:58:00 +0700

    Clear storage key

--- frontend/constant/storage-keys.constant.ts
+++ frontend/constant/storage-keys.constant.ts
@@ -2,14 +2,11 @@ const STORAGE_KEYS = {
   THEME: 'mezon_theme',
   LANGUAGE: 'mezon_language',
   SEARCH_HISTORY: 'mezon_search_history',
-  ACCESS_TOKEN: 'access_token',
   AUTH_TOKEN: 'auth_token',
   TOKEN: 'token',
-  REFRESH_TOKEN: 'refresh_token',
   USER_INFO: 'user_info',
   KEY_PAIR: 'key_pair',
   ZK_PROOF: 'zkProof',
-  CAMPAIGN_DRAFT: 'campaign_draft',
 } as const;
 
 export { STORAGE_KEYS };
