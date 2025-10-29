65cfb7b819727e7d43983150e9b81e577c6c273a
Laugh <phongvu2182@gmail.com>
2025-10-29 15:14:33 +0700

    add ? to make sure value is json

--- frontend/service/index.ts
+++ frontend/service/index.ts
@@ -23,7 +23,7 @@ const apiDongClient = axios.create({
 // Add interceptor for authentication
 apiDongClient.interceptors.request.use((config) => {
   const tokenAccess = localStorage.getItem(STORAGE_KEYS.TOKEN);
-  const accessToken = tokenAccess ? safeJsonParse(tokenAccess).access_token : null;
+  const accessToken = tokenAccess ? safeJsonParse(tokenAccess)?.access_token : null;
   const token = typeof window !== 'undefined' ? accessToken : null;
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
