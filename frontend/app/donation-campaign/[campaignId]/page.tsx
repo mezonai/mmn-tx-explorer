4f8b2886ca98bf1cc8ff339cd978d5468e0aba2e
tuyen.nguyenngoc <111561620+tuyen-nguyenngoc@users.noreply.github.com>
2025-10-30 16:01:23 +0700

    Validate store the wallet offline (#198)
    * Validate store the wallet offline
    
    * Update campaign detail title

--- frontend/app/donation-campaign/[campaignId]/page.tsx
+++ frontend/app/donation-campaign/[campaignId]/page.tsx
@@ -1,5 +1,8 @@
+import NotFound from '@/app/not-found';
+import { DonationCampaignService } from '@/modules/donation-campaign';
 import { CampaignDetail } from '@/modules/donation-campaign/components';
 import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
 interface CampaignDetailPageProps {
   params: Promise<{
@@ -9,13 +12,33 @@ interface CampaignDetailPageProps {
 
 export async function generateMetadata({ params }: CampaignDetailPageProps): Promise<Metadata> {
   const { campaignId } = await params;
+  try {
+    const campaign = await DonationCampaignService.getCampaignById(campaignId);
 
-  return {
-    title: `Campaign ${campaignId}`,
-  };
+    return {
+      title: campaign.name || `Campaign ${campaignId}`,
+      description: campaign.description,
+    };
+  } catch (error) {
+    return {
+      title: `Campaign ${campaignId}`,
+    };
+  }
 }
 
 export default async function DonationCampaignDetailPage({ params }: CampaignDetailPageProps) {
-  const { campaignId } = await params;
-  return <CampaignDetail campaignId={campaignId} />;
+  try {
+    const { campaignId } = await params;
+    const campaign = await DonationCampaignService.getCampaignById(campaignId);
+    return <CampaignDetail campaign={campaign} />;
+  } catch (error: unknown) {
+    if (error && typeof error === 'object' && 'response' in error) {
+      const httpError = error as { response: { status: number } };
+      if (httpError.response.status === 404) {
+        notFound();
+      }
+    }
+
+    throw error;
+  }
 }
