c2aa3591a0cad650dc30b80b7f16f8641e3f0541
viet.dangquoc <viet.dangquoc@ncc.asia>
2025-11-11 17:12:05 +0700

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,11 +30,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-       <link
+        <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-          integrity="sha512-+1dQKQbQbFZpK1iD7agEXkNKOxPQ0PqSxkbZVqhlV+3I8I4JVXu2fLG3gNsYch0eYRqpMfrQa6RlmK8t2VQ6Xg==" 
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
           crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
         />
       </head>
       <body className={`${inter.variable} antialiased`}>
