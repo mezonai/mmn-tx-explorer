747281938f9184edf13474cf66cb696cdddb2192
Laugh <phongvu2182@gmail.com>
2025-11-12 10:20:43 +0700

    new login flow

--- frontend/app/oauth2/login/route.ts
+++ frontend/app/oauth2/login/route.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+  const { searchParams, origin } = new URL(request.url);
   const state = searchParams.get('state');
+  if (!state) {
+    console.error('Missing state,');
+    return NextResponse.redirect(`${origin}/?error=missing_state`);
+  }
   const authUrl =
     `${process.env.NEXT_PUBLIC_OAUTH2_API_URL}/oauth2/auth?` +
     `client_id=${process.env.NEXT_PUBLIC_OAUTH2_CLIENT_ID}&` +
@@ -12,5 +16,11 @@ export async function GET(request: Request) {
     `state=${state}`;
 
   const response = NextResponse.redirect(authUrl);
+  response.cookies.set('state', state, {
+    httpOnly: true,
+    maxAge: 60 * 5,
+    path: '/',
+    sameSite: 'lax',
+  });
   return response;
 }
