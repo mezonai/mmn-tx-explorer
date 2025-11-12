d403cd786b268cece73bbabb20734013ffa48e66
Laugh <phongvu2182@gmail.com>
2025-11-12 19:47:57 +0700

    new callback file

--- frontend/app/oauth2/callback/route.ts
+++ frontend/app/oauth2/callback/route.ts
@@ -2,16 +2,26 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
+  const { searchParams } = new URL(request.url);
   const code = searchParams.get('code');
   const state = searchParams.get('state');
   const cookieStore = await cookies();
   const storeState = cookieStore.get('state');
-  if (!code || !state || state !== storeState?.value) {
-    console.error('Missing code, state, or CSRF token in storage.');
-    return NextResponse.redirect(`${origin}/?error='missing_auth_code_state_or_CSRF_token_in_storage.`);
+  const pathName = process.env.NEXT_BASE_FE;
+  if (!state) {
+    console.error('Missing state');
+    return NextResponse.redirect(`${pathName}/?error='missing_state`);
   }
+  if (state !== storeState?.value) {
+    console.error('Missing CSRF token in storage.');
+    return NextResponse.redirect(`${pathName}/?error='missing_CSRF_token_in_storage.`);
+  }
+  if (!code) {
+    console.error('Missing code');
+    return NextResponse.redirect(`${pathName}/?error='missing_code`);
+  }
+
   const originalState = JSON.parse(Buffer.from(state, 'base64').toString());
-  const redirect_url = `${origin}${originalState.redirect_url}/?authCode=${code}`;
+  const redirect_url = `${pathName}${originalState.redirect_url}/?authCode=${code}`;
   return NextResponse.redirect(redirect_url);
 }
