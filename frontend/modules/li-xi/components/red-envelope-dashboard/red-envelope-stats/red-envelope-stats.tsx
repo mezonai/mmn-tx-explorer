e61189bb45d461b11ee0aa8be705052e130565b1
thanh.phanvan <thanh.phanvan@ncc.asia>
2025-11-19 13:27:00 +0700

    308 update title

--- frontend/modules/li-xi/components/red-envelope-dashboard/red-envelope-stats/red-envelope-stats.tsx
+++ frontend/modules/li-xi/components/red-envelope-dashboard/red-envelope-stats/red-envelope-stats.tsx
@@ -51,7 +51,7 @@ export const RedEnvelopeStats = () => {
   return (
     <section className="">
       <div className="">
-        <PageHeader title="Mezon Red Envelope" header="Dashboard" />
+        <PageHeader title="Mezon Lucky Money" header="Dashboard" />
         <div className="mt-10 grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 sm:gap-6">
           {statsCards.map((item) => {
             const TitleIcon = item.titleIcon;
