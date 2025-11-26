2472aace42a5d75c018878556e802565a15d13b7
viet.dangquoc <viet.dangquoc@ncc.asia>
2025-11-26 10:32:19 +0700

    resolve conflict

--- indexer/cmd/api.go
+++ indexer/cmd/api.go
@@ -86,6 +86,7 @@ func RunAPI(cmd *cobra.Command, args []string) {
 	root.GET("/transactions", handlers.GetTransactions)
 	root.GET("/pending-transactions", handlers.GetPendingTransactions)
 	root.GET("/pending-tx/:transaction_hash/detail", handlers.GetPendingTransactionDetail)
+	root.GET("/export-transactions-csv", handlers.ExportTransactionsCSV)
 
 	// wallet queries
 	root.GET("/wallets", handlers.GetWallets)
