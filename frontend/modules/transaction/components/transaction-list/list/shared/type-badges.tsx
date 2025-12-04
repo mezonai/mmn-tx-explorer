02ffba69ec1cac0742dc629a9bb1c23395041aa5
viet.dangquoc <viet.dangquoc@ncc.asia>
2025-12-04 09:22:23 +0700

    Update type-badges.tsx

--- frontend/modules/transaction/components/transaction-list/list/shared/type-badges.tsx
+++ frontend/modules/transaction/components/transaction-list/list/shared/type-badges.tsx
@@ -15,6 +15,7 @@ interface TypeBadgesSkeletonProps {
 const getTransactionTypeInfo = (type: ETransferType) => {
   switch (type) {
     case ETransferType.TokenTransfer:
+    case ETransferType.DongGiveCoffee:
       return 'bg-yellow-100 text-yellow-600 dark:bg-yellow-500/10 dark:text-yellow-300';
     case ETransferType.DonationCampaign:
       return 'bg-green-100 text-green-600 dark:bg-green-500/10 dark:text-green-300';
@@ -24,7 +25,6 @@ const getTransactionTypeInfo = (type: ETransferType) => {
       return 'bg-pink-100 text-pink-600 dark:bg-pink-500/10 dark:text-pink-300';
     case ETransferType.GiveCoffee:
       return 'bg-orange-100 text-orange-600 dark:bg-orange-500/10 dark:text-orange-300';
-    case ETransferType.DongGiveCoffee:
     default:
       return 'bg-red-100 text-red-600 dark:bg-red-500/10 dark:text-red-300';
   }
