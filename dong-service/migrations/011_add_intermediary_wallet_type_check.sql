d501662e1224ab7fc5e1d2d59936289e9c2d6992
viet.dangquoc <viet.dangquoc@ncc.asia>
2025-12-09 12:01:22 +0700

    Create 011_add_intermediary_wallet_type_check.sql

---
+++ dong-service/migrations/011_add_intermediary_wallet_type_check.sql
@@ -0,0 +1,4 @@
+-- Add a CHECK constraint to ensure intermediary_wallet.type is one of the allowed values
+ALTER TABLE intermediary_wallet
+ADD CONSTRAINT chk_intermediary_wallet_type
+    CHECK (type IN ('DEFAULT', 'LUCKY_MONEY', 'OFFER'));
