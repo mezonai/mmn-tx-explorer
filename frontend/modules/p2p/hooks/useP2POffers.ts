a78c1addb38f948ee6229e43c0558b058c0ac4e7
Tran Duong Quang <tranduongquang10a5tqd@gmail.com>
2025-12-10 10:59:31 +0700

    Update useP2POffers.ts

--- frontend/modules/p2p/hooks/useP2POffers.ts
+++ frontend/modules/p2p/hooks/useP2POffers.ts
@@ -1,12 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
-import { IPaginatedResponse } from '@/types';
-import { IP2POfferListParams, P2POffer } from '../types';
 import { P2PService } from '../api';
 import { P2P_QUERY_KEYS } from '../constants';
+import { IP2POfferListParams } from '../types';
 
 export const useP2POffers = (params: IP2POfferListParams) => {
-  return useQuery<IPaginatedResponse<P2POffer[]>>({
+  return useQuery({
     queryKey: [P2P_QUERY_KEYS.OFFERS, params],
     queryFn: () => P2PService.getOffers(params),
+    enabled: !!params,
   });
 };
