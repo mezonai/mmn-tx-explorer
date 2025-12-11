a8307648e9ddd85b6464e8b1d314d4e4e1d8d4a9
Đỗ Anh Thư <athur4c@gmail.com>
2025-12-11 15:49:21 +0700

    [dong][FE} #461 fix type badge for donation feed

--- frontend/modules/transaction/utils.ts
+++ frontend/modules/transaction/utils.ts
@@ -4,8 +4,6 @@ export const getTransactionTypeLabel = (type: ETransactionType): string => {
   switch (type) {
     case ETransactionType.TokenTransfer:
       return 'Token Transfer';
-    case ETransactionType.DonationCampaign:
-      return 'Donation Campaign';
     case ETransactionType.WithdrawCampaign:
       return 'Withdraw Campaign';
     case ETransactionType.DonationFeed:
