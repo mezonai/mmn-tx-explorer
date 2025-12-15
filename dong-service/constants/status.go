2ae1651665cccc5da8a2190d534da16ef6e19f97
viet.dangquoc <viet.dangquoc@ncc.asia>
2025-12-15 18:02:02 +0700

    Update status.go

--- dong-service/constants/status.go
+++ dong-service/constants/status.go
@@ -28,14 +28,6 @@ const (
 	TransactionStatusFINALIZED int16 = 2
 )
 
-// Blockchain Transaction Status Constants (from MMN SDK)
-const (
-	TxStatusPending   int32 = 0 // Transaction is pending
-	TxStatusConfirmed int32 = 1 // Transaction is confirmed
-	TxStatusFinalized int32 = 2 // Transaction is finalized
-	TxStatusFailed    int32 = 3 // Transaction failed
-)
-
 const (
 	RedEnvelopeStatusPending   = "PENDING"   // Awaiting transaction confirmation
 	RedEnvelopeStatusPublished = "PUBLISHED" // Active and claimable
@@ -94,19 +86,6 @@ const (
 
 const (
 	TextDataLuckyMoney = "Lucky Money fund"
-)
-
-const (
-	TrandingOpen     = "OPEN"
-	TradingPending   = "PENDING"
-	TradingConfirmed = "CONFIRMED"
-	TradingCanceled  = "CANCELED"
-	TradingFailed    = "FAILED"
-)
-
-const (
-	PriceTypeFixed = "FIXED"
-	PriceTypeFloat = "FLOAT"
 )
 
 // GetStatusName returns the human-readable name for a status code
