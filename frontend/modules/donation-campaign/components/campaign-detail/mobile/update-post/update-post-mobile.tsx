e80ead022e9778b6cbd77674db93f0102886cfda
Đỗ Anh Thư <athur4c@gmail.com>
2025-12-15 15:48:51 +0700

    [dong][FE] #462 change post UI

---
+++ frontend/modules/donation-campaign/components/campaign-detail/mobile/update-post/update-post-mobile.tsx
@@ -0,0 +1,106 @@
+import { Chip } from '@/components/shared';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import { DonationCampaign, IDonationFeed } from '@/modules/donation-campaign/type';
+import { ClientTimeDisplay } from '@/modules/transaction/components/transaction-details/shared/client-time-display';
+import { Popover, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover';
+import { VersionHistoryDialog } from '../../shared/donation-feed';
+import { TxnHashLink } from '@/modules/transaction/components/transaction-list/list/shared';
+import { useRouter } from 'next/navigation';
+import { ROUTES } from '@/configs/routes.config';
+import { JSX } from 'react';
+import { useUser } from '@/providers';
+import { Pencil } from 'lucide-react';
+
+interface UpdatePostMobileProps {
+  update: IDonationFeed;
+  campaign: DonationCampaign;
+  onImageClick: (url: string) => void;
+  getImages: (imageCids: string[], onImageClick: (url: string) => void) => JSX.Element;
+  isVersionDialogOpen: boolean;
+  setIsVersionDialogOpen: (isOpen: boolean) => void;
+}
+
+export const UpdatePostMobile = ({
+  update,
+  campaign,
+  onImageClick,
+  getImages,
+  isVersionDialogOpen,
+  setIsVersionDialogOpen,
+}: UpdatePostMobileProps) => {
+  const { user } = useUser();
+  const router = useRouter();
+  return (
+    <Card className={`dark:bg-card border-muted-foreground/30 gap-4 rounded-3xl bg-white/90 pt-3 shadow-sm`}>
+      <div className="flex flex-col gap-2 md:hidden">
+        <div className="flex items-center justify-between px-2">
+          <div className="px-2 text-xs text-gray-400">
+            <ClientTimeDisplay timestamp={new Date(update.created_at).getTime()} />
+          </div>
+          {(user?.walletAddress === update.creator_address || update.parent_hash) && (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="link"
+                  className="text-muted-foreground hover:text-brand-primary px-2 text-xs font-thin hover:no-underline"
+                >
+                  •••
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="bg-background w-auto p-1" align="end">
+                <div className="flex flex-col">
+                  {user?.walletAddress === update.creator_address && (
+                    <Button
+                      variant="ghost"
+                      className="h-8 justify-start text-sm font-normal"
+                      onClick={() => router.push(ROUTES.EDIT_DONATION_UPDATE(campaign.slug, String(update.id)))}
+                    >
+                      <Pencil className="text-primary mr-2 h-4 w-4" />
+                      Edit
+                    </Button>
+                  )}
+                  {update.parent_hash && (
+                    <VersionHistoryDialog
+                      update={update}
+                      isOpen={isVersionDialogOpen}
+                      onOpenChange={setIsVersionDialogOpen}
+                      onImageClick={onImageClick}
+                    />
+                  )}
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
+        </div>
+
+        <div className="flex flex-row flex-wrap items-center gap-2 px-2">
+          <Chip variant="success">On chain</Chip>
+          {update.parent_hash && (
+            <Chip variant="warning">
+              <span>Edited</span>
+            </Chip>
+          )}
+        </div>
+      </div>
+      <div className="text-foreground text-md flex w-full flex-col px-4">
+        <h3 className="text-lg font-semibold break-words text-gray-900 dark:text-white">{update.title}</h3>
+        {update.description.split('\n').map((line, index) => (
+          <span key={index} className="text-foreground mt-2 text-sm break-words">
+            {line}
+            {index < update.description.split('\n').length - 1 && <br />}
+          </span>
+        ))}
+      </div>
+
+      {getImages(update.image_cids || [], onImageClick)}
+
+      <div className="flex w-full flex-row justify-end gap-4 px-4">
+        <span className="text-sm">TxHash: </span>
+        <span className="w-40">
+          <TxnHashLink hash={update.tx_hash} isPending={false} className="text-brand-primary" />
+        </span>
+      </div>
+    </Card>
+  );
+};
--- frontend/modules/donation-campaign/components/campaign-detail/shared/donation-feed/update-post.tsx
+++ frontend/modules/donation-campaign/components/campaign-detail/shared/donation-feed/update-post.tsx
@@ -1,20 +1,12 @@
 'use client';
 
 import { DonationCampaign, IDonationFeed } from '@/modules/donation-campaign';
-import { Card } from '@/components/ui/card';
-import { Chip } from '@/components/shared';
-import { CopyButton } from '@/components/ui/copy-button';
-import { ClientTimeDisplay } from '@/modules/transaction/components/transaction-details/shared/client-time-display';
-import { TxnHashLink } from '@/modules/transaction/components/transaction-list/list/shared';
 import { ipfsServiceURL } from '@/service';
-import { VersionHistoryDialog } from './version-history-dialog';
 import { useState } from 'react';
-import { useUser } from '@/providers';
-import { Button } from '@/components/ui/button';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { ROUTES } from '@/configs/routes.config';
-import { useRouter } from 'next/navigation';
-import { Pencil } from 'lucide-react';
+import { UpdatePostDesktop } from '../../desktop/update-post';
+import { UpdatePostMobile } from '../../mobile/update-post';
+import { useBreakpoint } from '@/hooks';
+import { EBreakpoint } from '@/enums';
 
 interface UpdatePostProps {
   update: IDonationFeed;
@@ -40,101 +32,28 @@ function getImages(imageCids: string[], onImageClick: (url: string) => void) {
 
 export const UpdatePost = ({ update, campaign, onImageClick }: UpdatePostProps) => {
   const [isVersionDialogOpen, setIsVersionDialogOpen] = useState(false);
-  const { user } = useUser();
-  const router = useRouter();
+  const isMobile = !(useBreakpoint(EBreakpoint.MD) ?? true);
   return (
-    <Card className={`dark:bg-card border-muted-foreground/30 gap-4 rounded-3xl bg-white/90 shadow-sm`}>
-      <div className="flex w-full flex-col justify-between gap-3 px-4 md:flex-row">
-        <div className="flex flex-row flex-wrap gap-2">
-          <Chip variant="brand" className="">
-            {update.title}
-          </Chip>
-
-          <div className="pt-2 text-xs text-gray-400">
-            <ClientTimeDisplay timestamp={new Date(update.created_at).getTime()} />
-          </div>
-          {update.creator_address && (
-            <div className="pt-2 text-xs text-gray-500 lg:block">
-              · posted by {update.creator_address.slice(0, 3)}...{update.creator_address.slice(-4)}{' '}
-              <CopyButton textToCopy={update.creator_address} />
-            </div>
-          )}
-          {update.parent_hash && (
-            <Chip variant="warning" className="text-xs">
-              Edited - new version on chain
-            </Chip>
-          )}
-        </div>
-
-        <div className="text-muted-foreground flex flex-row gap-1 text-xs">
-          <span className="inline-flex items-center gap-1">
-            <span className="h-1.5 w-1.5 rounded-full bg-emerald-400"></span>
-            <p>On chain</p>
-          </span>
-
-          {(user?.walletAddress === update.creator_address || update.parent_hash) && (
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant="link"
-                  className="text-muted-foreground hover:text-brand-primary p-0 pl-2 text-xs font-thin hover:no-underline"
-                >
-                  •••
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="bg-background w-auto p-1" align="end">
-                <div className="flex flex-col">
-                  {user?.walletAddress === update.creator_address && (
-                    <>
-                      <Button
-                        variant="ghost"
-                        className="h-8 justify-start text-sm font-normal"
-                        onClick={() => router.push(ROUTES.EDIT_DONATION_UPDATE(campaign.slug, String(update.id)))}
-                      >
-                        <Pencil className="text-primary mr-2 h-4 w-4" />
-                        Edit
-                      </Button>
-                      {/* <Button
-                        variant="ghost"
-                        className="h-8 justify-start text-sm font-normal"
-                        onClick={() => {}}
-                      >
-                        <EyeClosed className="text-primary mr-2 h-4 w-4" />
-                        Hide
-                      </Button> */}
-                    </>
-                  )}
-                  {update.parent_hash && (
-                    <VersionHistoryDialog
-                      update={update}
-                      isOpen={isVersionDialogOpen}
-                      onOpenChange={setIsVersionDialogOpen}
-                      onImageClick={onImageClick}
-                    />
-                  )}
-                </div>
-              </PopoverContent>
-            </Popover>
-          )}
-        </div>
-      </div>
-      <div className="text-foreground text-md w-full px-4 break-words">
-        {update.description.split('\n').map((line, index) => (
-          <span key={index}>
-            {line}
-            {index < update.description.split('\n').length - 1 && <br />}
-          </span>
-        ))}
-      </div>
-
-      {getImages(update.image_cids || [], onImageClick)}
-
-      <div className="flex w-full flex-row justify-end gap-4 px-4">
-        <span className="text-sm">TxHash: </span>
-        <span className="w-40">
-          <TxnHashLink hash={update.tx_hash} isPending={false} className="text-brand-primary" />
-        </span>
-      </div>
-    </Card>
+    <>
+      {isMobile ? (
+        <UpdatePostMobile
+          update={update}
+          campaign={campaign}
+          onImageClick={onImageClick}
+          getImages={getImages}
+          isVersionDialogOpen={isVersionDialogOpen}
+          setIsVersionDialogOpen={setIsVersionDialogOpen}
+        />
+      ) : (
+        <UpdatePostDesktop
+          update={update}
+          campaign={campaign}
+          onImageClick={onImageClick}
+          getImages={getImages}
+          isVersionDialogOpen={isVersionDialogOpen}
+          setIsVersionDialogOpen={setIsVersionDialogOpen}
+        />
+      )}
+    </>
   );
 };
