4e9dca9f8e6924bb319f6dda44a12c366ecd777a
hieudinhngoc-ncc <hieu.dinhngoc@ncc.asia>
2025-12-24 09:01:23 +0700

    533: remove update clause in migration

--- dong-service/migrations/019_add_unique_constraint_to_offers_hash.sql
+++ dong-service/migrations/019_add_unique_constraint_to_offers_hash.sql
@@ -1,11 +1,2 @@
-UPDATE dong_schema.offers
-SET transaction_hash = NULL
-WHERE offer_id NOT IN (
-    SELECT MAX(offer_id)
-    FROM dong_schema.offers
-    WHERE transaction_hash IS NOT NULL
-    GROUP BY transaction_hash
-) AND transaction_hash IS NOT NULL;
-
 ALTER TABLE dong_schema.offers
 ADD CONSTRAINT uq_offers_transaction_hash UNIQUE (transaction_hash);
