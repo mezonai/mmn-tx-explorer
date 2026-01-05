5d721b2627f8da3cd9fb43e94bc6d9ebbcb940df
Trịnh Việt Phương <phuong.trinhviet@ncc.asia>
2026-01-05 15:20:49 +0700

    [dong][FE]#574  campaign - prevent access edit campaign via URL

--- frontend/modules/donation-campaign/components/create-and-edit/edit_campaign.tsx
+++ frontend/modules/donation-campaign/components/create-and-edit/edit_campaign.tsx
@@ -4,17 +4,26 @@ import { CampaignSidebar } from './campaign-sidebar';
 import { CampaignHeader } from './campaign-header';
 import { Separator } from '@/components/ui/separator';
 import { CampaignForm } from './campaign-form';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { useCampaign } from '../../hooks';
 import { useEffect } from 'react';
+import { useUser } from '@/providers/AppProvider';
+import { ROUTES } from '@/configs/routes.config';
 
 function EditCampaignContent() {
   const params = useParams<{ slug: string }>();
   const slug = params?.slug ? String(params.slug) : '';
   const { data: campaign } = useCampaign(slug);
   const { updateField } = useCreateCampaignContext();
+  const { user } = useUser();
+  const router = useRouter();
 
   useEffect(() => {
+    if (campaign && user && user.id !== campaign.creator) {
+      router.replace(ROUTES.CAMPAIGN(campaign.slug));
+      return;
+    }
+
     if (campaign) {
       updateField('name', campaign.name || '');
       updateField('shortDescription', campaign.description || '');
@@ -46,6 +55,17 @@ export const EditCampaign = () => {
   const params = useParams<{ slug: string }>();
   const slug = params?.slug ? String(params.slug) : '';
   const { data: campaign } = useCampaign(slug);
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (campaign && user && user.id !== campaign.creator) {
+      router.replace(ROUTES.CAMPAIGN(campaign.slug));
+    }
+  }, [campaign, router, user]);
+
+  if (campaign && user && user.id !== campaign.creator) return null;
+
   return (
     <CreateCampaignProvider id={campaign?.id}>
       <EditCampaignContent />
