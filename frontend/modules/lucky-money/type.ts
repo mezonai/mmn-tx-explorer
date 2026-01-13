6df2ab34254c6750c3d3a4e3f645fb14650d691a
Trịnh Việt Phương <phuong.trinhviet@ncc.asia>
2026-01-13 13:43:16 +0700

    [dong] #598 return total_envelopes

--- frontend/modules/lucky-money/type.ts
+++ frontend/modules/lucky-money/type.ts
@@ -94,7 +94,7 @@ export interface UpdateStatusRedEnvelopeRequest {
 
 export interface ClaimEnvelopeRequest {
   id: UUID;
-  split_money_id: number;     
+  split_money_id: number;
 }
 
 export interface RedEnvelopeClaim {
@@ -126,6 +126,6 @@ export interface RedEnvelopeDetailRecipient {
 }
 
 export interface RedEnvelopeStats {
-  total_active_envelopes: number;
+  total_envelopes: number;
   total_claimed: number;
 }
