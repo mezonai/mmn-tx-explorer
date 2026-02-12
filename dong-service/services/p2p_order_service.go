d8099e36826328645c60362288d9f9990cc02e42
Tran Duong Quang <tranduongquang10a5tqd@gmail.com>
2026-02-12 20:21:20 +0700

    rollback

--- dong-service/services/p2p_order_service.go
+++ dong-service/services/p2p_order_service.go
@@ -116,6 +116,10 @@ func (s *OrderService) CreateOrder(ctx context.Context, offerID int64, req *mode
 	}
 
 	order.BankInfo = offer.BankInfo
+	order.OfferCreatorWalletAddress = offer.OfferCreatorWalletAddress
+	order.OfferCreatorUserID = offer.OfferCreatorUserID
+	order.PriceRate = offer.PriceRate
+	order.OfferSide = &offer.Side
 
 	return order, offer, nil
 }
@@ -135,6 +139,10 @@ func (s *OrderService) ListOrdersByOffer(ctx context.Context, offerID int64, pag
 	if err == nil && of != nil {
 		for i := range orders {
 			orders[i].BankInfo = of.BankInfo
+			orders[i].OfferCreatorWalletAddress = of.OfferCreatorWalletAddress
+			orders[i].OfferCreatorUserID = of.OfferCreatorUserID
+			orders[i].PriceRate = of.PriceRate
+			orders[i].OfferSide = &of.Side
 		}
 	}
 
@@ -151,6 +159,10 @@ func (s *OrderService) GetOrderByID(ctx context.Context, id int64) (*models.Orde
 		of, err := s.offerRepo.GetOfferByID(ctx, *o.OfferID)
 		if err == nil && of != nil {
 			o.BankInfo = of.BankInfo
+			o.OfferCreatorWalletAddress = of.OfferCreatorWalletAddress
+			o.OfferCreatorUserID = of.OfferCreatorUserID
+			o.PriceRate = of.PriceRate
+			o.OfferSide = &of.Side
 		}
 	}
 
@@ -172,6 +184,10 @@ func (s *OrderService) GetOrdersByWalletAddress(ctx context.Context, walletAddre
 			of, err := s.offerRepo.GetOfferByID(ctx, *orders[i].OfferID)
 			if err == nil && of != nil {
 				orders[i].BankInfo = of.BankInfo
+				orders[i].OfferCreatorWalletAddress = of.OfferCreatorWalletAddress
+				orders[i].OfferCreatorUserID = of.OfferCreatorUserID
+				orders[i].PriceRate = of.PriceRate
+				orders[i].OfferSide = &of.Side
 			}
 		}
 	}
