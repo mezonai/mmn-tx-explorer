17641d3576ea4372fa08eaf543187b64db2fe866
Tran Duong Quang <tranduongquang10a5tqd@gmail.com>
2026-02-13 15:48:34 +0700

    fix syntax

--- dong-service/services/p2p_order_service.go
+++ dong-service/services/p2p_order_service.go
@@ -145,7 +145,7 @@ func (s *OrderService) GetOrderByID(ctx context.Context, id int64) (*models.Orde
 		if err == nil && of != nil {
 			o.BankInfo = of.BankInfo
 			o.OfferCreatorWalletAddress = &of.OfferCreatorWalletAddress
-			o.OfferCreatorUserID = of.OfferCreatorUserID
+			o.OfferCreatorUserID = &of.OfferCreatorUserID
 			o.PriceRate = of.PriceRate
 		}
 	}
@@ -169,7 +169,7 @@ func (s *OrderService) GetOrdersByWalletAddress(ctx context.Context, walletAddre
 			if err == nil && of != nil {
 				orders[i].BankInfo = of.BankInfo
 				orders[i].OfferCreatorWalletAddress = &of.OfferCreatorWalletAddress
-				orders[i].OfferCreatorUserID = of.OfferCreatorUserID
+				orders[i].OfferCreatorUserID = &of.OfferCreatorUserID
 				orders[i].PriceRate = of.PriceRate
 			}
 		}
