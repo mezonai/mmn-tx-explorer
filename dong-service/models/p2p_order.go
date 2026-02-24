c9bfda6890fea844cba32af6cabe48e813b55771
Tran Duong Quang <tranduongquang10a5tqd@gmail.com>
2026-02-24 09:13:17 +0700

    rollback

--- dong-service/models/p2p_order.go
+++ dong-service/models/p2p_order.go
@@ -32,6 +32,9 @@ type Order struct {
 	CreatedAt                 time.Time          `json:"created_at" db:"created_at"`
 	UpdatedAt                 time.Time          `json:"updated_at" db:"updated_at"`
 	BankInfo                  *string            `json:"bank_info,omitempty" db:"-"`
+	OfferCreatorWalletAddress *string            `json:"offer_creator_wallet_address,omitempty" db:"-"`
+	OfferCreatorUserID        *string            `json:"offer_creator_user_id,omitempty" db:"-"`
+	PriceRate                 *float64           `json:"price_rate,omitempty" db:"-"`
 }
 
 type CreateOrderRequest struct {
