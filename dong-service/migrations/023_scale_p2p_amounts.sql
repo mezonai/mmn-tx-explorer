931a6a610a7fa04b2ae04ec151f32e9461b35f3e
viet.dangquoc <viet.dangquoc@ncc.asia>
2026-02-27 09:58:44 +0700

    Update 023_scale_p2p_amounts.sql

--- dong-service/migrations/023_scale_p2p_amounts.sql
+++ dong-service/migrations/023_scale_p2p_amounts.sql
@@ -1,25 +1,3 @@
--- Drop any ghost triggers that might be using old column names (e.g. "amount")
-DO $$ 
-DECLARE
-    r RECORD;
-BEGIN
-    FOR r IN (
-        SELECT trigger_name, event_object_table 
-        FROM information_schema.triggers 
-        WHERE event_object_schema = 'dong_schema' 
-        AND (event_object_table = 'p2p_offers' OR event_object_table = 'p2p_orders')
-    ) LOOP
-        EXECUTE 'DROP TRIGGER IF EXISTS ' || r.trigger_name || ' ON dong_schema.' || r.event_object_table;
-    END LOOP;
-END $$;
-
--- Scale p2p_offers amounts by 1,000,000
-UPDATE dong_schema.p2p_offers
-SET available_amount = available_amount * 1000000,
-    total_amount = total_amount * 1000000,
-    min_amount = min_amount * 1000000,
-    max_amount = max_amount * 1000000;
-
 -- Scale p2p_orders amounts by 1,000,000
 UPDATE dong_schema.p2p_orders
 SET order_amount = order_amount * 1000000;
