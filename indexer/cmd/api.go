a1af291546efc9647b7706de9a44d12cc73fd365
Hikaru2035 <phungngocdiep05@gmail.com>
2026-03-18 08:03:59 +0000

    fix watchdog.go

--- indexer/cmd/api.go
+++ indexer/cmd/api.go
@@ -160,23 +160,3 @@ func registerPprof() {
 		}
 	}()
 }
-
-func startSystemdWatchdog() {
-	interval, err := daemon.SdWatchdogEnabled(false)
-	if err != nil || interval == 0 {
-		return
-	}
-
-	log.Info().
-		Dur("interval", interval).
-		Msg("Systemd watchdog enabled")
-
-	go func() {
-		ticker := time.NewTicker(interval / 2)
-		defer ticker.Stop()
-
-		for range ticker.C {
-			daemon.SdNotify(false, daemon.SdNotifyWatchdog)
-		}
-	}()
-}
