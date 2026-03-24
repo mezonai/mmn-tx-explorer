c0da4531f06027cf95725fb013de89ed3acf7940
viet.dangquoc <84317678+vietdangquoc@users.noreply.github.com>
2026-03-24 09:35:56 +0700

    Update 029_drop_unique_bank_account_number_constraint.sql

--- dong-service/migrations/029_drop_unique_bank_account_number_constraint.sql
+++ dong-service/migrations/029_drop_unique_bank_account_number_constraint.sql
@@ -3,7 +3,5 @@ DROP CONSTRAINT IF EXISTS user_payment_info_bank_name_account_number_key;
 
 ALTER TABLE dong_schema.user_payment_info ADD COLUMN IF NOT EXISTS deleted_at TIMESTAMPTZ;
 
-DROP INDEX IF EXISTS dong_schema.idx_unique_primary_per_user;
-CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_primary_per_user
 ON dong_schema.user_payment_info (user_id)
 WHERE is_primary = true AND deleted_at IS NULL;
