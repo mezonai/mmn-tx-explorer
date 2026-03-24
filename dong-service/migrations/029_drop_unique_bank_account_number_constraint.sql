d434604028b72b37248366729962abda31066db8
viet.dangquoc <84317678+vietdangquoc@users.noreply.github.com>
2026-03-24 13:42:18 +0700

    Update 029_drop_unique_bank_account_number_constraint.sql

--- dong-service/migrations/029_drop_unique_bank_account_number_constraint.sql
+++ dong-service/migrations/029_drop_unique_bank_account_number_constraint.sql
@@ -2,6 +2,3 @@ ALTER TABLE dong_schema.user_payment_info
 DROP CONSTRAINT IF EXISTS user_payment_info_bank_name_account_number_key;
 
 ALTER TABLE dong_schema.user_payment_info ADD COLUMN IF NOT EXISTS deleted_at TIMESTAMPTZ;
-
-ON dong_schema.user_payment_info (user_id)
-WHERE is_primary = true AND deleted_at IS NULL;
