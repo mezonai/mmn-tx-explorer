21627c343a0713fa4786ced14c34bf4dde964c4f
viet.dangquoc <84317678+vietdangquoc@users.noreply.github.com>
2026-04-15 15:40:49 +0700

    Update utils.ts

--- frontend/modules/lucky-money/utils.ts
+++ frontend/modules/lucky-money/utils.ts
@@ -70,16 +70,11 @@ export const getStatusDisplay = (
           dark:border-gray-600 dark:bg-yellow-900/50 dark:text-yellow-400`,
       };
     case 'expired':
+    case 'closed':
       return {
         text: 'Closed',
         className: `border bg-yellow-100 text-yellow-700 border-gray-200 
           dark:border-gray-600 dark:bg-yellow-700/30 dark:text-yellow-400`,
-      };
-    case 'closed':
-      return {
-        text: 'Closed',
-        className: `border bg-gray-200 text-gray-700 border-gray-200 
-          dark:border-gray-600 dark:bg-slate-700 dark:text-gray-400`,
       };
     case 'failed':
       return {
